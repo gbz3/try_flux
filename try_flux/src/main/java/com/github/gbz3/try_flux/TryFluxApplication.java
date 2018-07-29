b8c2eacb1906119086c96f9c7f766d60b04dbb2d
gbz3 <gebizo3@gmail.com>
2018-07-29 13:00:58 +0900

    start

---
+++ try_flux/src/main/java/com/github/gbz3/try_flux/TryFluxApplication.java
@@ -0,0 +1,12 @@
+package com.github.gbz3.try_flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryFluxApplication.class, args);
+	}
+}
