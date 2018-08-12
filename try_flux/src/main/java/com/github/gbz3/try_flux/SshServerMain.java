6fd9f8e80e48ebe0c911487f8a4127b2061e1589
gbz3 <gebizo3@gmail.com>
2018-08-12 10:33:00 +0900

    Give up

--- try_flux/src/main/java/com/github/gbz3/try_flux/SshServerMain.java
+++ try_flux/src/main/java/com/github/gbz3/try_flux/SshServerMain.java
@@ -1,11 +1,17 @@
 package com.github.gbz3.try_flux;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.sshd.common.channel.RequestHandler;
+import org.apache.sshd.common.session.ConnectionService;
+import org.apache.sshd.common.util.GenericUtils;
+import org.apache.sshd.common.util.buffer.Buffer;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.auth.pubkey.AcceptAllPublickeyAuthenticator;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
-import org.apache.sshd.server.shell.ProcessShellFactory;
+import org.apache.sshd.server.shell.InteractiveProcessShellFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,12 +24,35 @@ public class SshServerMain {
 		sshd.setKeyPairProvider( new SimpleGeneratorHostKeyProvider() );
 		sshd.setPublickeyAuthenticator( AcceptAllPublickeyAuthenticator.INSTANCE );
 //		sshd.setShellFactory( new ProcessShellFactory( new String[] { "cmd.exe" } ) );
-		sshd.setShellFactory( new ProcessShellFactory( new String[] { "bash.exe", "-i", "-l" } ) );
+//		sshd.setShellFactory( new ProcessShellFactory( new String[] { "bash.exe", "-i", "-l" } ) );
 //		sshd.setShellFactory( new ProcessShellFactory( new String[] { "/bin/sh", "-i", "-l" } ) );
+		final InteractiveProcessShellFactory ipsf = InteractiveProcessShellFactory.INSTANCE;
+		//ipsf.setCommand( Arrays.asList( "dir.exe" ) );
+		sshd.setShellFactory( ipsf );
+		//sshd.setCommandFactory( new UnknownCommandFactory() );
+//		sshd.setCommandFactory(new ScpCommandFactory.Builder()
+//	            .withDelegate(ProcessShellCommandFactory.INSTANCE)
+//	            .build());
+		//sshd.setSubsystemFactories(null);
+		
+		final List<RequestHandler<ConnectionService>> oldGlobals = sshd.getGlobalRequestHandlers();
+		final List<RequestHandler<ConnectionService>> newGlobals = new ArrayList<>();
+		if( GenericUtils.size( oldGlobals ) > 0) {
+			newGlobals.addAll( oldGlobals );
+		}
+		newGlobals.add( new RequestHandler<ConnectionService>() {
+			@Override public Result process( ConnectionService t, String request, boolean wantReply, Buffer buffer )	throws Exception {
+				logger.info( "t={} request={} wantReply={} buffer={}", t, request, wantReply, buffer );
+				return null;
+			}
+		});
+		logger.info( "requestHandler={}", newGlobals );
 		
 		sshd.start();
 		
 		Thread.sleep( 60L * 1000L );
 	}
+	
+	
 
 }
